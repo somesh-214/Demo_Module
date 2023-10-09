7d72af2b8ca68272113fd570e6f320c4fe548941
somesh-214 <Someshsingh2009@gmail.com>
2023-10-09 21:10:45 +0530

    this is new add file rg

---
+++ Web_App/main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = var.app_serviceplan_name
+  resource_group_name = var.rg_name
+  location            = var.location
+ sku_name = "B3"
+    os_type             = "Windows"
+}
+
+resource "azurerm_app_service" "web_app" {
+  name                = var.web_app_name
+  location            = var.location
+  resource_group_name = var.rg_name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+   
+  }
+}
+
