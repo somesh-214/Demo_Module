ab06be6405678df3e6440843dcfe36eac78f5637
somesh-214 <145552505+somesh-214@users.noreply.github.com>
2023-10-11 00:40:45 +0530

    Update main.tf

--- Web_App/main.tf
+++ Web_App/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_app_service" "web_app" {
   name                = var.web_app_name
   location            = var.location
   resource_group_name = var.rg_name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
 
   site_config {
    
